d1d1db9c0a303c166c2ef0c0c9bc3deffd6296cd
Ali Jaddoa <a.h.jaddoa@gmail.com>
2025-09-24 11:31:10 +0100

    comit

---
+++ hello_test.py
@@ -0,0 +1,4 @@
+import hello 
+
+def assess_hello():
+    assert hello.hello_world() == "Hello World!"
---
+++ sumsation_test.py
@@ -0,0 +1,7 @@
+import summation
+
+def test_summation():
+
+   # It’s basically saying: “Check that the result of summation.summation(4) is equal to 5. If it isn’t, raise an error.” 
+    assert summation.summation(4) == 5
+
